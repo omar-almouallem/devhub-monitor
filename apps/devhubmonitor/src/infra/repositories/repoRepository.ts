61afebccda3904c960ba18c3557e96d1fbb1b50a
OmarElMouallem <elmouallemomar7@gmail.com>
2024-10-29 02:34:11 +0300

    Add pagination methods for retrieving repositories by user

--- apps/devhubmonitor/src/infra/repositories/repoRepository.ts
+++ apps/devhubmonitor/src/infra/repositories/repoRepository.ts
@@ -2,6 +2,8 @@ import { IRepository, IRepoRepository } from '@dev-hub-monitor/types';
 
 import RepoModel from '../../models/repositories';
 
+import { paginate } from './services/repoPaginationService';
+
 export class MongoRepoRepository implements IRepoRepository {
   async saveRepository (
     userId: string,
@@ -25,8 +27,36 @@ export class MongoRepoRepository implements IRepoRepository {
     return repo;
   }
 
-  async getReposByUser (userId: string): Promise<IRepository[]> {
-    const repos = await RepoModel.find({ userId: userId });
-    return repos;
+  async getRepoNames (userId: string, cursor?: string) {
+    const reposQuery = RepoModel.find(
+      { userId },
+      {
+        unique_key: 1,
+      },
+    );
+    const paginatedResult = await paginate(reposQuery, cursor);
+
+    return {
+      results: paginatedResult.results,
+      nextCursor: paginatedResult.nextCursor,
+      hasMore: paginatedResult.hasMore,
+    };
+  }
+
+  async getUserRepos (
+    userId: string,
+    cursor?: string,
+  ): Promise<{
+    results: IRepository[];
+    nextCursor: string | null;
+    hasMore: boolean;
+  }> {
+    const reposQuery = RepoModel.find({ userId });
+    const paginatedResult = await paginate(reposQuery, cursor);
+    return {
+      results: paginatedResult.results,
+      nextCursor: paginatedResult.nextCursor,
+      hasMore: paginatedResult.hasMore,
+    };
   }
 }
