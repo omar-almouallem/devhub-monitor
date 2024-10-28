85d00e2890159561235c82ce16a82b6f75f1344f
OmarElMouallem <elmouallemomar7@gmail.com>
2024-10-28 21:57:09 +0300

    Enhance IRepoRepository interface with cursor-based pagination for repositories

--- libs/types/src/db/repository/repoRepository.interface.ts
+++ libs/types/src/db/repository/repoRepository.interface.ts
@@ -3,5 +3,13 @@ import { IRepository } from '../../dto';
 export interface IRepoRepository {
   saveRepository(userId: string, repo: IRepository): Promise<IRepository>;
   getRepoByKey(uniqueKey: string): Promise<IRepository>;
-  getReposByUser(userId: string): Promise<IRepository[]>;
+  getRepoNames(userId: string, cursor?: string);
+  getUserRepos(
+    userId: string,
+    cursor?: string,
+  ): Promise<{
+    results: IRepository[];
+    nextCursor: string | null;
+    hasMore: boolean;
+  }>;
 }
