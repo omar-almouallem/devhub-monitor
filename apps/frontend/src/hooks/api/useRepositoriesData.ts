4b7ddd939909c69a366b38988dccff9e37e0ef34
OmarElMouallem <elmouallemomar7@gmail.com>
2024-10-29 03:07:57 +0300

    Remove fetching logic and simplify state management

--- apps/frontend/src/hooks/api/useRepositoriesData.ts
+++ apps/frontend/src/hooks/api/useRepositoriesData.ts
@@ -1,28 +1,11 @@
 import { useState, useEffect } from 'react';
 
-import { getUserData } from '../../services/userData.service';
-
 const useFetchUserData = (token: string | null) => {
-  const [userData, setUserData] = useState<any>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const [userData, setUserData] = useState<any>(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (token) {
-        try {
-          const fetchedUserData = await getUserData();
-          setUserData(fetchedUserData);
-        } catch (error) {
-          console.error('Failed to fetch user data:', error);
-          setError('Failed to fetch user data');
-        }
-      }
-      setLoading(false);
-    };
-
-    fetchData();
-  }, [token]);
+  
 
   return { userData, loading, error };
 };
